28e49f6a8a281cfb6675248cd760841633c5f7a4
Ramesh Vyas <rameshovyas@gmail.com>
2023-10-26 13:36:10 +0530

    Prepared the ViewModel for MainPage created methods and commands needed in the MainPage view

--- MonkeyFinder/MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ MonkeyFinder/MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -1,6 +1,52 @@
-﻿namespace MonkeyFinder.ViewModel
+﻿using MonkeyFinder.Model;
+using MonkeyFinder.Services;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+
+namespace MonkeyFinder.ViewModel
 {
     public class MonkeysViewModel : BaseViewModel
     {
+        public ObservableCollection<Monkey> Monkeys = new();
+        public Command GetMonkeysCommand { get; }
+        MonkeyService monkeyService;
+        public MonkeysViewModel(MonkeyService monkeyService) 
+        {
+            Title = "Monkey Finder";
+            this.monkeyService = monkeyService;
+            GetMonkeysCommand = new Command(async () => await GetMonkeysAsync());
+        }
+
+        async Task GetMonkeysAsync()
+        {
+            if(IsBusy) 
+               return;
+
+            try
+            {
+                IsBusy = true;
+
+                var monkeys = await monkeyService.GetMonkeys();
+                
+                if (Monkeys.Count != 0)
+                    Monkeys.Clear();
+
+                foreach (var monkey in monkeys)
+                    Monkeys.Add(monkey);
+            }
+
+            catch(Exception ex) 
+            {
+                Debug.WriteLine($"Unable to get monkeys : {ex.Message}");
+                await Application.Current.MainPage.DisplayAlert("Error!", ex.Message, "Ok");
+            }
+
+            finally
+            {
+                IsBusy = false;
+            }
+
+        }
+
     }
 }
