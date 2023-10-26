0be79f44ebe8cc40bab6609899444190f3aa51b0
Ramesh Vyas <rameshovyas@gmail.com>
2023-10-26 11:59:42 +0530

    added general properties in the BaseViewModel that will be inherited in other ViewModels

--- MonkeyFinder/MonkeyFinder/ViewModel/BaseViewModel.cs
+++ MonkeyFinder/MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -5,6 +5,31 @@ namespace MonkeyFinder.ViewModel
 {
     public class BaseViewModel : INotifyPropertyChanged
     {
+        bool isBusy;
+        string title;
+
+        public bool IsBusy
+        {
+            get => isBusy;
+            set
+            {
+                if(isBusy == value) return;
+                isBusy = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Title
+        {
+            get => title;
+            set
+            {
+                if (title == value) return;
+                title = value;
+                OnPropertyChanged();
+            }
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         public void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
